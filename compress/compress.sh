4a7f392aaa18fa7b513cbac337b2530388322249
郭磊 <guolei@gdxsoft.com>
2021-03-13 21:19:57 +0800

    js compress

--- compress/compress.sh
+++ compress/compress.sh
@@ -1,32 +1,39 @@
 #!/bin/sh
 
 echo create EWA.js
-cd `dirname $0`
-echo 当前目录：$(pwd)
+sh_dir=`dirname $0`
 
-cat src/core/*.js  > EWA.js
+src=${sh_dir}/../src/main/resources/EWA_STYLE/js/source/src
+target=${sh_dir}/../src/main/resources/EWA_STYLE/js/source
+
+echo   DIR：${sh_dir}
+echo   SRC：${src}
+echo TARGET：${target}
+
+cat ${src}/core/*.js  > ${target}/EWA.js
 
 echo create EWA_UI.js
-cat src/ui/*.js > EWA_UI.js
+cat ${src}/ui/*.js > ${target}/EWA_UI.js
 
 echo create EWA_FRAME.js
-cat src/frames/*.js > EWA_FRAME.js
+cat ${src}/frames/*.js > ${target}/EWA_FRAME.js
 
 echo create EWA_MISC.js
-cat src/misc/*.js > EWA_MISC.js
+cat ${src}/misc/*.js > ${target}/EWA_MISC.js
 
 echo create EWA_ALL.js
-cat EWA.js EWA_UI.js EWA_FRAME.js  EWA_MISC.js > EWA_ALL.js
+cat ${target}/EWA.js ${target}/EWA_UI.js ${target}/EWA_FRAME.js  ${target}/EWA_MISC.js > ${target}/EWA_ALL.js
 
 echo start compress with google 
 
-
 #echo start compress EWA_ALL.js
-java -jar compiler.jar --js EWA_ALL.js --js_output_file EWA_ALL.min.compiler.js --create_source_map EWA_ALL.min.2.0.js.map
+java -jar ${sh_dir}/compiler.jar --js ${target}/EWA_ALL.js --js_output_file ${target}/EWA_ALL.min.js --create_source_map ${target}/EWA_ALL.min.map
 
+echo "//# sourceMappingURL=EWA_ALL.min.map" >> ${target}/EWA_ALL.min.js
 echo start combine EWA_ALL.min.2.0.js
-mv EWA_ALL.min.compiler.js EWA_ALL.min.2.0.js
-echo "//# sourceMappingURL=/EmpScriptV2/EWA_STYLE/js/js_jquery/EWA_ALL.min.2.0.js.map" >> EWA_ALL.min.2.0.js
+mv ${target}/EWA_ALL.min.js ${target}/../EWA_ALL.min.js
+mv ${target}/EWA_ALL.js ${target}/../EWA_ALL.js
+mv ${target}/EWA_ALL.min.map ${target}/../EWA_ALL.min.map
 
 
 
